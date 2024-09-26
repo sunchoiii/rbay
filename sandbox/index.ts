6f222a75c07ca72185fac1aa56bee49c4451fd5a
Sun choi <csun2772@naver.com>
2024-09-26 23:39:07 +0900

    git

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,19 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950
+	});
+
+	const car = await client.hGetAll('car#234234');
+
+	if (Object.keys(car).length === 0) {
+		      console.log('404 Error');
+					return;
+	}
+
+	console.log(car);
+};
 run();
